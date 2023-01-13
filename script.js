f2f5925ce14132bdb15c2b70302f17a666c19b18
Erik-Varga <viglew@gmail.com>
2023-01-13 18:07:38 -0500

    v1.1 update

--- script.js
+++ script.js
@@ -9,25 +9,30 @@ const orderTripleBtn = document.getElementById("order-triple");
 const cancelBtn = document.getElementById("cancel");
 
 function singleQuote() {
-    quoteText.textContent = '$300';
+    quoteText.textContent = '$300 Single';
     orderSingleBtn.innerText = 'Selected';
     orderDoubleBtn.innerText = 'Select';
     orderTripleBtn.innerText = 'Select';
-
+    cancelBtn.innerText = 'X';
+    cancelBtn.style.display = 'flex';
 }
 
 function doubleQuote() {
-    quoteText.textContent = '$500';
+    quoteText.textContent = '$500 Double';
     orderSingleBtn.innerText = 'Select';
     orderDoubleBtn.innerText = 'Selected';
     orderTripleBtn.innerText = 'Select';
+    cancelBtn.innerText = 'X';
+    cancelBtn.style.display = 'flex';
 }
 
 function tripleQuote() {
-    quoteText.textContent = '$600';
+    quoteText.textContent = '$600 Triple';
     orderSingleBtn.innerText = 'Select';
     orderDoubleBtn.innerText = 'Select';
     orderTripleBtn.innerText = 'Selected';
+    cancelBtn.innerText = 'X';
+    cancelBtn.style.display = 'flex';
 }
 
 function cancelQuote() {
@@ -35,7 +40,8 @@ function cancelQuote() {
     orderSingleBtn.innerText = 'Select';
     orderDoubleBtn.innerText = 'Select';
     orderTripleBtn.innerText = 'Select';
-    cancelBtn.innerText = 'x';
+    cancelBtn.innerText = '';
+    cancelBtn.style.display = 'none';
 }
 
 orderSingleBtn.addEventListener('click', singleQuote);
