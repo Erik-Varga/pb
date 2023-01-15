5f145c2541b614e26233fe4038541508f9547c4f
Erik-Varga <67766949+Erik-Varga@users.noreply.github.com>
2023-01-15 09:16:05 -0500

    Updated HTML, CSS, JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const quoteText = document.querySelector(".quote");
-// const quoteAmount = document.querySelector(".currentQuote");
+const amountText = document.getElementById('amt_user');
 
 const orderSingleBtn = document.getElementById("order-single");
 const cancelSingleBtn = document.getElementById("cancel-single");
@@ -9,30 +9,33 @@ const orderTripleBtn = document.getElementById("order-triple");
 const cancelBtn = document.getElementById("cancel");
 
 function singleQuote() {
-    quoteText.textContent = '$300 Single';
+    quoteText.textContent = 'Total Pushy Price: $300';
     orderSingleBtn.innerText = 'Selected';
     orderDoubleBtn.innerText = 'Select';
     orderTripleBtn.innerText = 'Select';
     cancelBtn.innerText = 'X';
     cancelBtn.style.display = 'flex';
+    amountText.value = "SinglePushy";
 }
 
 function doubleQuote() {
-    quoteText.textContent = '$500 Double';
+    quoteText.textContent = 'Total Pushy Price: $500';
     orderSingleBtn.innerText = 'Select';
     orderDoubleBtn.innerText = 'Selected';
     orderTripleBtn.innerText = 'Select';
     cancelBtn.innerText = 'X';
     cancelBtn.style.display = 'flex';
+    amountText.value = "DoublePushy";
 }
 
 function tripleQuote() {
-    quoteText.textContent = '$600 Triple';
+    quoteText.textContent = 'Total Pushy Price: $600';
     orderSingleBtn.innerText = 'Select';
     orderDoubleBtn.innerText = 'Select';
     orderTripleBtn.innerText = 'Selected';
     cancelBtn.innerText = 'X';
     cancelBtn.style.display = 'flex';
+    amountText.value = "TriplePushy";
 }
 
 function cancelQuote() {
@@ -42,6 +45,7 @@ function cancelQuote() {
     orderTripleBtn.innerText = 'Select';
     cancelBtn.innerText = '';
     cancelBtn.style.display = 'none';
+    amountText.value = "0";
 }
 
 orderSingleBtn.addEventListener('click', singleQuote);
